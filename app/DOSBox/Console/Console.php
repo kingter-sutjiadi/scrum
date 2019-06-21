be9b218fd7e6c280f105483e5f5b1b733ac1962a
kingter sutjiadi <kgr_zz@hotmail.com>
2019-06-21 16:20:20 +0700

    - fix bug on exit

--- app/DOSBox/Console/Console.php
+++ app/DOSBox/Console/Console.php
@@ -24,7 +24,7 @@ class Console {
 
         $line = "";
 
-        while(strcmp(trim($line), "exit") != 0 || strcmp(trim($line), "exit gugus") != 0){
+        while(strcmp(trim($line), "exit") != 0 && strcmp(trim($line), "exit gugus") != 0){
             $this->outputter->newLine();
             $this->outputter->printNoLine($this->drive->getPrompt());
 
@@ -36,9 +36,11 @@ class Console {
                 // do nothing by intention
             }
 
-            // $this->invoker->executeCommand($line, $this->outputter);
+            $this->invoker->executeCommand($line, $this->outputter);
         }
 
-        $this->outputter->printLine("Goodbye!");
+        // if(strcmp(trim($line), "exit") == 0) {
+            $this->outputter->printLine("Goodbye!");
+        // }
     }
 }
