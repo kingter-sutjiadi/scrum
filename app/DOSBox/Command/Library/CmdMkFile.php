9934f24feb959ab2215c6c953df3858e62c86912
arfian_bagus <arfianbagus@gmail.com>
2019-06-21 17:31:39 +0700

    file exist

--- app/DOSBox/Command/Library/CmdMkFile.php
+++ app/DOSBox/Command/Library/CmdMkFile.php
@@ -23,13 +23,38 @@ class CmdMkFile extends Command {
 
     public function execute(IOutputter $outputter){
         $fileName = $this->params[0];
-        if (count($this->params) > 1){
-            $fileContent = $this->params[1];
-            $newFile = new File($fileName, $fileContent);
+
+        $dirContent =$this->getDrive()->getCurrentDirectory()->getContent();
+        // $outputter->printLine(count($dirContent));
+        if (count($dirContent) > 0){
+            foreach ($dirContent as $item) {
+                // $outputter->printLine($item->getName());
+                if ($this->chopExtension($item->getName()) == $this->chopExtension($fileName)){
+                    $outputter->printLine("File sama");
+                } else {
+                    if (count($this->params) > 1){
+                        $fileContent = $this->params[1];
+                        $newFile = new File($fileName, $fileContent);
+                    } else {
+                        $newFile = new File($fileName, null);
+                    }
+                    $this->getDrive()->getCurrentDirectory()->add($newFile);
+                }
+            }
         } else {
-            $newFile = new File($fileName, null);
+            if (count($this->params) > 1){
+                $fileContent = $this->params[1];
+                $newFile = new File($fileName, $fileContent);
+            } else {
+                $newFile = new File($fileName, null);
+            }
+            $this->getDrive()->getCurrentDirectory()->add($newFile);
         }
-        $this->getDrive()->getCurrentDirectory()->add($newFile);
+        
+    }
+
+    public function chopExtension($filename) {
+        return substr($filename, 0, strrpos($filename, '.'));
     }
 
 }
